e5d6eb8dd41db37b7b77a13f9eddd42972ec4d2a
raiomido <raiomido@gmail.com>
2017-03-09 23:51:31 +0300

    Add files via upload

---
+++ tests/Unit/TransactionControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TransactionControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testIndex() {
+        $this->assertTrue(true);
+    }
+
+    public function testDeposit() {
+
+        $response = $this->call('GET', 'account/manage/deposit');
+        $this->assertRedirectedToAction('TransactionController@create', null, ['flash_message']);
+
+        // Only check that you're redirecting to a specific URI
+        $this->assertRedirectedTo('account/manage');
+
+        // Just check that you don't get a 200 OK response.
+        $this->assertFalse($response->isOk());
+
+        // Make sure you've been redirected.
+        $this->assertTrue($response->isRedirection());
+    }
+}
