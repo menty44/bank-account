b793902cabf6cd9d5b811af4237157cbf093405e
raiomido <raiomido@gmail.com>
2017-03-10 04:32:25 +0300

    Update transactions.blade.php

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -6,21 +6,24 @@
 @section('content')
 @if($transactions)
 <ul class="list-group">
-@foreach($transactions as $transaction)  
-<li class="list-group-item list-group-item-info">
-<?php
-if($transaction['type'] ===1){
-$type = 'deposited';
-$a = 'into';
-}elseif($transaction['type'] === 0){
-$type = 'withdrew';
-$a = 'from';
-}
-echo "You $type &#x24;<strong>". abs($transaction['amount'])."</strong> ". $a;
-?>
- your bank account on date <strong> {{$transaction['created_at']}}</strong> 
-</li>
-@endforeach
-<ul>
-@endif
-@endsection
+    @foreach($transactions as $transaction)  
+    <li class="list-group-item list-group-item-info">
+        <?php
+        if ($transaction['type'] === 2) {
+            $type = 'deposited';
+            $a = 'into';
+        } elseif ($transaction['type'] === 1) {
+            $type = 'withdrew';
+            $a = 'from';
+        } else {
+            $type = '';
+            $a = '';
+        }
+        echo "You $type &#x24;<strong>" . abs($transaction['amount']) . "</strong> " . $a;
+        ?>
+        your bank account on date <strong> {{$transaction['created_at']}}</strong> 
+    </li>
+    @endforeach
+    <ul>
+        @endif
+        @endsection
